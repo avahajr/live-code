8ebb4d8ce28686dfa07aa59d6ef5a8fecd43c181
Ava Hajratwala <100488972+avahajr@users.noreply.github.com>
2023-11-26 22:14:17 -0500

    final submission

--- src/App.js
+++ src/App.js
@@ -1,40 +1,183 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import React, { useEffect, useState } from "react";
-import { Button, Container, Form } from "react-bootstrap";
-// import FormText from "react-bootstrap/Form";
+import React from "react";
+import { Button, Container } from "react-bootstrap";
 import "./main.css";
-import { reevaluate } from "./audioLogic";
-const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-const dynamicsCompressorNode = audioCtx.createDynamicsCompressor();
-
+// import { reevaluate } from "./audioLogic";
+// sample input: sine: 3@350 1@500;
+// sawtooth: 2@200;
+// square: 4@600
 function App() {
-  useEffect(() => {
-    const playButton = document.getElementById("playButton");
+  let audioCtx;
+  let dc;
+  var liveCodeState = [];
+  var numLines = 0;
+  var activeOscs = [];
+  var activeGains = [];
 
-    const handleClick = () => {
-      console.log("doing reevaluate");
-      reevaluate(audioCtx, dynamicsCompressorNode);
-    };
+  // Check if the audioCtx is already created
+  function initAudio() {
+    // alert("intializing audio");
+    audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    dc = audioCtx.createDynamicsCompressor();
 
-    playButton.addEventListener("click", handleClick);
+    // Set the new audioCtx in the state
+    scheduleAudio();
+  }
 
-    return () => {
-      playButton.removeEventListener("click", handleClick);
-    };
-  }, []); // Empty dependency array ensures that the effect runs only once on mount
+  const handleButtonClick = () => {
+    if (!audioCtx) {
+      initAudio();
+    }
+    console.log("reevaluate");
+    reevaluate();
+  };
+
+  function scheduleAudio() {
+    let longestLine = 0;
+    for (let i = 0; i < liveCodeState.length; i++) {
+      let timeElapsedSecs = 0;
+      let line = liveCodeState[i];
+      if (line.length !== 0) {
+        // each line should have its own oscillator
+        const timings = activeGains[i];
+        const osc = activeOscs[i];
+        osc.type = line[0]["waveform"];
+        osc.connect(timings).connect(dc).connect(audioCtx.destination);
+        for (let j = 0; j < line.length; j++) {
+          let noteData = line[j];
+          timings.gain.setTargetAtTime(
+            1,
+            audioCtx.currentTime + timeElapsedSecs,
+            0.01
+          );
+          osc.frequency.setTargetAtTime(
+            noteData["pitch"],
+            audioCtx.currentTime + timeElapsedSecs,
+            0.01
+          );
+          timeElapsedSecs += noteData["length"] / 10.0;
+          timings.gain.setTargetAtTime(
+            0,
+            audioCtx.currentTime + timeElapsedSecs,
+            0.01
+          );
+          timeElapsedSecs += 0.2; //rest between notes
+
+          if (timeElapsedSecs > longestLine) {
+            longestLine = timeElapsedSecs;
+          }
+        }
+      }
+    }
+    setTimeout(scheduleAudio, longestLine * 1000);
+  }
+  function parseCode(code) {
+    // const code = document.getElementById("code").value;
+    // Updates the line count and returns an array of arrays of objects.
+    if (code === "") {
+      numLines = 0;
+      return [];
+    }
+    let lines = code.split(";\n");
+
+    lines = lines.map((line) => {
+      // sine: <rest of line here>
+      let wave = line.split(":")[0];
+      console.log(wave);
+      let notes = line.split(" ");
+      if (
+        wave === "triangle" ||
+        wave === "sine" ||
+        wave === "sawtooth" ||
+        wave === "square"
+      ) {
+        notes.shift(); // the first is the wa
+      }
+      console.log(notes);
+
+      return notes.map((note) => {
+        let noteData = note.split("@");
+        return {
+          waveform: wave,
+          // eslint-disable-next-line
+          length: eval(noteData[0]), // The 'eval' function allows us to write JS code in our live coding language
+          // eslint-disable-next-line
+          pitch: eval(noteData[1]),
+        };
+      });
+    });
+    numLines = lines.length;
+    console.log("numlines", numLines);
+    // alert(lines);
+    return lines;
+  }
+  function reevaluate() {
+    // first, call parsecode to get the new livecode state (LCS)
+    const code = document.getElementById("code").value;
+    const newData = parseCode(code);
+    liveCodeState = newData;
+    // now, make sure that there are the correct number of oscillators/gainNodes
+    // this helps us get ready for the change in LCS
+    // console.log(numLines, "lines,", activeOscs.length, "activeOscs");
+    if (numLines > activeOscs.length) {
+      while (numLines > activeOscs.length) {
+        const newOsc = audioCtx.createOscillator();
+        newOsc.start();
+        activeOscs.push(newOsc);
+        activeGains.push(audioCtx.createGain());
+        // console.log("currently active gain nodes:", activeGains.length);
+        // console.log("currently active osc nodes:", activeOscs.length);
+      }
+    } else if (numLines < activeOscs.length) {
+      while (numLines < activeOscs.length) {
+        activeGains.pop().gain.setValueAtTime(0, audioCtx.currentTime);
+        const removedOsc = activeOscs.pop();
+        removedOsc.stop();
+
+        // console.log("currently active gain nodes:", activeGains.length);
+        // console.log("currently active osc nodes:", activeOscs.length);
+      }
+    }
+    console.log("updated livecode state:", liveCodeState);
+    console.log("active oscs:", activeOscs);
+  }
 
   return (
     <>
       <Container style={{ marginTop: "5rem" }}>
         <h1> Lab 4: Live coding</h1>
+        <h5 style={{ marginBottom: "2rem" }}>Ava Hajratwala</h5>
+        <hr></hr>
+        <h3>Basic syntax</h3>
         <p>
-          Add more than one line (<b>separate lines with a semicolon</b>) to
-          have two patterns repeating at once.
+          <code className="mx-auto">
+            waveform: length@pitch length@pitch ...
+          </code>
+        </p>
+        <p>and so on, as long as you want the sequence to be.</p>
+        <h2 style={{ marginTop: "2rem" }}>Features</h2>
+        <p>
+          1. Separate lines with a semicolon to have two patterns repeating at
+          once.
+        </p>
+        <p>
+          2. Pick the line's waveform by labeling it with a <code>:</code>{" "}
+          (defaults to sine wave).
         </p>
       </Container>
       <Container className="text-center" style={{ marginTop: "2rem" }}>
-        <Form.Control id="code" as="textarea" />
-        <Button className="mt-4 playButton" size="lg" id="playButton">
+        <textarea
+          className="form-control"
+          id="code"
+          placeholder={
+            "i.e.,\nsine: 3@350 1@500;\nsawtooth: 2@200;\nsquare: 4@600\n\nand so on..."
+          }
+        />
+        <Button
+          className="mt-4 playButton"
+          size="lg"
+          onClick={handleButtonClick}
+        >
           RE-COMPILE
         </Button>
       </Container>
--- src/audioLogic.js
+++ src/audioLogic.js
@@ -1,44 +1,47 @@
 var liveCodeState = [];
 var activeOscs = [];
 var activeGains = [];
-var numLines = 0;
-// const playButton = document.getElementById("playButton");
-
-export function initAudio() {
-  const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-  // const dynamicsCompressorNode = audioCtx.createDynamicsCompressor();
-  return audioCtx;
-}
+var numLines = 0; // max is 10
 
 function scheduleAudio(line, osc, gainNode, audioCtx) {
   let timeElapsedSecs = 0;
   // console.log("line has type", typeof line, line);
+  if (!osc) {
+    return;
+  }
+  if (liveCodeState.includes(line)) {
+    // console.log("livecode state", liveCodeState, "includes", line);
+    line.forEach((noteData) => {
+      // console.log(
+      //   "Processing note with length",
+      //   noteData["length"],
+      //   "and pitch",
+      //   noteData["pitch"]
+      // );
 
-  line.forEach((noteData) => {
-    // console.log("noteData has type", typeof noteData, noteData);
-    gainNode.gain.setTargetAtTime(
-      1,
-      audioCtx.currentTime + timeElapsedSecs,
-      0.01
-    );
-    osc.frequency.setTargetAtTime(
-      noteData["pitch"],
-      audioCtx.currentTime + timeElapsedSecs,
-      0.01
-    );
-    timeElapsedSecs += noteData["length"] / 10.0;
-    gainNode.gain.setTargetAtTime(
-      0,
-      audioCtx.currentTime + timeElapsedSecs,
-      0.01
-    );
-    timeElapsedSecs += 0.2; //rest between notes
+      gainNode.gain.setTargetAtTime(
+        1,
+        audioCtx.currentTime + timeElapsedSecs,
+        0.01
+      );
+      osc.frequency.setTargetAtTime(
+        noteData["pitch"],
+        audioCtx.currentTime + timeElapsedSecs,
+        0.01
+      );
+      timeElapsedSecs += noteData["length"] / 10.0;
+      gainNode.gain.setTargetAtTime(
+        0,
+        audioCtx.currentTime + timeElapsedSecs,
+        0.01
+      );
+      timeElapsedSecs += 0.2; //rest between notes
 
-    setTimeout(
-      () => scheduleAudio(line, osc, gainNode, audioCtx),
-      timeElapsedSecs * 1000
-    );
-  });
+      setTimeout(() => scheduleAudio(), timeElapsedSecs * 1000);
+    });
+  } else {
+    alert("ending recursive call");
+  }
 }
 
 function parseCode(code) {
@@ -47,9 +50,11 @@ function parseCode(code) {
   // How could we allow for two lines that play at the same time?
   // What if we want variables?
   // How does this parsing technique limit us?
+
   if (code === "") {
-    return null;
+    return [];
   }
+
   let lines = code.split(";\n");
   lines = lines.map((line) => {
     let notes = line.split(" ");
@@ -74,18 +79,9 @@ function parseCode(code) {
 
 function genAudio(data, audioCtx) {
   liveCodeState = data;
-  numLines = Object.keys(data).length;
+  numLines = data.length;
   console.log("numLines:", numLines, ",", "# active oscs:", activeOscs.length);
   if (numLines > activeOscs.length) {
-    adjustNumOscsAndGains(1, audioCtx); // add more oscilators
-  } else if (numLines < activeOscs.length) {
-    adjustNumOscsAndGains(0, audioCtx); // remove some oscilators
-  }
-}
-
-function adjustNumOscsAndGains(increase, audioCtx) {
-  if (increase) {
-    // console.log("increase");
     while (numLines > activeOscs.length) {
       const newOsc = audioCtx.createOscillator();
       newOsc.start();
@@ -94,43 +90,36 @@ function adjustNumOscsAndGains(increase, audioCtx) {
       // console.log("currently active gain nodes:", activeGains.length);
       // console.log("currently active osc nodes:", activeOscs.length);
     }
-  } else {
-    // console.log("decrease");
+  } else if (numLines < activeOscs.length) {
     while (numLines < activeOscs.length) {
-      const removedGainNode = activeGains.pop();
-      removedGainNode.setTargetAtTime(0, audioCtx.currentTime);
+      activeGains.pop().gain.setValueAtTime(0, audioCtx.currentTime);
       const removedOsc = activeOscs.pop();
-      removedOsc.stop(audioCtx.currentTime + 0.1);
+      removedOsc.stop();
       removedOsc.disconnect();
+
       // console.log("currently active gain nodes:", activeGains.length);
       // console.log("currently active osc nodes:", activeOscs.length);
     }
   }
-  // console.log("finishing adjustNumOscsAndGains");
-  // console.log("numLines:", numLines);
-  // console.log("currently active gain nodes:", activeGains.length);
-  // console.log("currently active osc nodes:", activeOscs.length);
 }
 
-export function reevaluate(audioCtx, dc) {
-  var code = document.getElementById("code").value;
+export function reevaluate(audioCtx, dc, code) {
+  // var code = document.getElementById("code").value;
   var data = parseCode(code);
 
-  if (data) {
-    genAudio(data, audioCtx); // modifies the liveCodeState, preps gain and oscs
-    console.log("currently active gain nodes:", activeGains.length);
-    console.log("currently active osc nodes:", activeOscs.length);
+  genAudio(data, audioCtx); // modifies the liveCodeState, preps gain and oscs
+  console.log("currently active gain nodes:", activeGains.length);
+  console.log("currently active osc nodes:", activeOscs.length);
 
-    var idx = 0;
-    liveCodeState.forEach((line) => {
-      const osc = activeOscs[idx];
-      const timings = activeGains[idx];
+  var idx = 0;
+  liveCodeState.forEach((line) => {
+    const osc = activeOscs[idx];
+    const timings = activeGains[idx];
 
-      timings.gain.value = 0;
-      osc.connect(timings).connect(dc).connect(audioCtx.destination);
-
-      scheduleAudio(line, osc, timings, audioCtx);
-      idx++;
-    });
-  }
+    timings.gain.value = 0;
+    osc.connect(timings).connect(dc).connect(audioCtx.destination);
+    console.log("starting recursive call for line:", idx);
+    scheduleAudio(line, osc, timings, audioCtx);
+    idx++;
+  });
 }
